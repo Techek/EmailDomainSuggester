6f6f187873dc881f37e01b6237c634d416ed134f
Jacob Kamp Lund <jacob.kamp.lund@dgi.dk>
2017-05-23 15:16:57 +0200

    Oprydning mellem mapper og namespaces

---
+++ Dgi.Email.Test/Services/EmailServiceSkal.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Dgi.Email.ApplicationService.Services;
+using Dgi.Email.Dal.Repositories;
+using Xunit;
+
+namespace Dgi.Email.Test.Services
+{
+    public class EmailServiceSkal
+    {
+        [Fact]
+        public void ReturnereAtEmailadresseMedMxDomaeneFindes()
+        {
+            // Arrange
+            string emailadresse = "jacob.kamp.lund@dgi.dk";
+            var emailService = new EmailService(new EmailRepository());
+
+            // Act
+            var result = emailService.Verify(emailadresse);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void ReturnereAtEmailadresseUdenMxDomaeneIkkeFindes()
+        {
+            // Arrange
+            string emailadresse = "jacob.kamp.lund@dgi" + Guid.NewGuid().ToString().Replace("-", "") + ".dk";
+            var emailService = new EmailService(new EmailRepository());
+
+            // Act
+            var result = emailService.Verify(emailadresse);
+
+            // Assert
+            Assert.False(result);
+        }
+    }
+}
